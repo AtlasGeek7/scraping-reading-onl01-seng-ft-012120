e4f350ec11278fe7427955e422e501e14bf61897
Khalid T <khalid.tissire@gmail.com>
2020-02-10 06:15:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,11 +8,16 @@ doc = Nokogiri::HTML(html)
 header = doc.css('.headline-26OIBN')
 puts header.text.strip
 
+puts "Courses: "
+4.times { |i| puts doc.search(".title-oE5vT4")[i].text.strip }
 
 
 
+=begin
 
 online_course = doc.css('.text-4GLMvr.title-oE5vT4')
 online_course.each do |course|
   puts course.text.strip
 end
+
+=end
